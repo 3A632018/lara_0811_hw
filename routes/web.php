bf555ed89f44e9b1ac230b5f9eb4a20406038cb5
3A632018 <f223145@gmail.com>
2019-10-18 20:22:32 +0800

    設定首頁內容加入兩個按鈕&修改HomeController內容

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ Route::get('/', ['as'=>'home.index','uses'=>'HomeController@index']);
 Route::get('/about', ['as'=>'about.index','uses'=>'AboutController@index']);
 Route::get('/news', ['as'=>'news.index','uses'=>'NewsController@index']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+    //return view('welcome');
+//});
 
 
