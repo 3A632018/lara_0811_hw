9c752ac8b944631d79004e2278d964fcaec5f504
3A632018 <f223145@gmail.com>
2019-10-18 20:09:34 +0800

    設定Route 裡的三個route name&URL

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', ['as'=>'home.index','uses'=>'HomeController@index']);
+Route::get('/about', ['as'=>'about.index','uses'=>'AboutController@index']);
+Route::get('/news', ['as'=>'news.index','uses'=>'NewsController@index']);
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
